fc4e764114979420753af233131e878179ef687f
Uday Verma <verma@ubuntu.(none)>
2013-01-30 18:17:27 -0800

    Add support for reading holding registers and write multiple registers

--- server.js
+++ server.js
@@ -11,40 +11,40 @@ var FUNCTION_CODES = modbus.FUNCTION_CODES;
  * is going to handle. 
  */
 function Server (handlers) {
-  net.Server.call(this, this._setupConn);
-  if (typeof handlers == 'function') {
-    this.on('request', handlers);
-  } else {
-    this.on('request', this._handler);
-    this.handlers = handlers || {};
-  }
+	net.Server.call(this, this._setupConn);
+	if (typeof handlers == 'function') {
+		this.on('request', handlers);
+	} else {
+		this.on('request', this._handler);
+		this.handlers = handlers || {};
+	}
 }
 require('util').inherits(Server, net.Server);
 module.exports = Server;
 
 Server.prototype._setupConn = function(stream) {
-  var self = this;
-  var response = new modbus.ModbusResponseStack(stream);
-  response.on('request', function(request) {
-    self.emit('request', request, response);
-    if (stream.readable && stream.writable) {
-      self._setupConn(stream);
-    }
-  });
+	var self = this;
+	var response = new modbus.ModbusResponseStack(stream);
+	response.on('request', function(request) {
+		self.emit('request', request, response);
+		if (stream.readable && stream.writable) {
+			self._setupConn(stream);
+		}
+	});
 }
 
 // Called for every 'request' event, when a "handlers" Object was passed.
 Server.prototype._handler = function(request, response) {
-  if (request.functionCode in this.handlers) {
-    this.handlers[request.functionCode].call(this, request, response);
-  } else {
-    response.writeException(1);
-  }
+	if (request.functionCode in this.handlers) {
+		this.handlers[request.functionCode].call(this, request, response);
+	} else {
+		response.writeException(1);
+	}
 }
 
 // Convenience function to create a MODBUS Server.
 function createServer (handlers) {
-  return new Server(handlers);
+	return new Server(handlers);
 }
 Server.createServer = createServer;
 
@@ -52,12 +52,12 @@ Server.createServer = createServer;
 // The pattern of 'startAddress' and 'quantity' is used by a lot of
 // MODBUS functions, and can be re-used in the code.
 function parseTwoWord16be(first, second) {
-  return function(bufferlist) {
-    return Binary(bufferlist)
-      .getWord16be(first)
-      .getWord16be(second)
-      .end().vars;
-  }
+	return function(bufferlist) {
+		return Binary(bufferlist)
+		.getWord16be(first)
+		.getWord16be(second)
+		.end().vars;
+	}
 }
 
 var no_parameters = function() { return {}; }
@@ -65,43 +65,69 @@ var startAddress_quantity = parseTwoWord16be('startAddress', 'quantity');
 var address_value = parseTwoWord16be('address', 'value');
 
 Server.REQUESTS = {
-  // READ_COILS
-  1: startAddress_quantity,
-  // READ_DISCRETE_INPUTS
-  2: startAddress_quantity,
-  // READ_HOLDING_REGISTERS
-  3: startAddress_quantity,
-  // READ_INPUT_REGISTERS
-  4: startAddress_quantity,
-  // WRITE_SINGLE_COIL
-  5: function(bufferlist) {
-    var rtn = address_value.call(this, bufferlist);
-    rtn.value = rtn.value === 0xff00;
-    return rtn;
-  },
-  // WRITE_SINGLE_REGISTER
-  6: address_value,
-  // READ_EXCEPTION_STATUS (Serial Line Only)
-  7: no_parameters,
-  // GET_COMM_EVENT_COUNTER (Serial Line Only)
-  11: no_parameters,
-  // GET_COMM_EVENT_LOG (Serial Line Only)
-  12: no_parameters,
-  // REPORT_SLAVE_ID (Serial Line Only)
-  17: no_parameters
+	// READ_COILS
+	1: startAddress_quantity,
+	// READ_DISCRETE_INPUTS
+	2: startAddress_quantity,
+	// READ_HOLDING_REGISTERS
+	3: startAddress_quantity,
+	// READ_INPUT_REGISTERS
+	4: startAddress_quantity,
+	// WRITE_SINGLE_COIL
+	5: function(bufferlist) {
+		var rtn = address_value.call(this, bufferlist);
+		rtn.value = rtn.value === 0xff00;
+		return rtn;
+	},
+	// WRITE_SINGLE_REGISTER
+	6: address_value,
+	// READ_EXCEPTION_STATUS (Serial Line Only)
+	7: no_parameters,
+	// GET_COMM_EVENT_COUNTER (Serial Line Only)
+	11: no_parameters,
+	// GET_COMM_EVENT_LOG (Serial Line Only)
+	12: no_parameters,
+	16: function(bufferlist) {
+		var ret = Binary(bufferlist)
+			.getWord16be('startAddress')
+			.getWord16be('registerCount')
+			.getWord8('byteCount')
+			.getBuffer('data', 'byteCount')
+			.end().vars;
+
+		ret.values = [];
+		for (var i = 0 ; i < ret.registerCount ; i ++) {
+			ret.values.push(ret.data[2*i] * 256 + ret.data[2*i+1]);
+		}
+
+		delete ret.byteCount;
+		delete ret.data;
+		return ret;
+	},
+	// REPORT_SLAVE_ID (Serial Line Only)
+	17: no_parameters
 };
 
+var genericWriter = function(registers) {
+	if (!Array.isArray(registers) || registers.length != this.request.quantity) {
+		throw new Error('Expected to write an "Array" of length "'+this.request.quantity+'"');
+	}
+	var i=0, l=registers.length, put = Put()
+	.word8(registers.length*2);
+	for (; i<l; i++) {
+		put.word16be(registers[i]);
+	}
+	return put.buffer();
+}
+
 Server.RESPONSES = {
-  // READ_INPUT_REGISTERS
-  4: function(registers) {
-    if (!Array.isArray(registers) || registers.length != this.request.quantity) {
-      throw new Error('Expected to write an "Array" of length "'+this.request.quantity+'"');
-    }
-    var i=0, l=registers.length, put = Put()
-      .word8(registers.length*2);
-    for (; i<l; i++) {
-      put.word16be(registers[i]);
-    }
-    return put.buffer();
-  }
+	// READ_INPUT_REGISTERS
+	3: genericWriter,
+	4: genericWriter,
+	16: function(ret) {
+		if (!Array.isArray(ret) || ret.length != 2) {
+			throw new Error('Expected to write an "Array" of length 2, first element: start of address, second element: total registers');
+		}
+		return Put().word16be(ret[0]).word16be(ret[1]).buffer();
+	}
 };
